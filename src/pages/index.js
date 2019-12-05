b9bf4dd204a4df7d502bea2820695f72edc78b1c
denny <denny.george90@gmail.com>
2019-12-05 18:42:06 +0100

    setup an index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,89 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import {Grommet, Box, Heading, Text, Table, TableHeader, TableBody, TableCell, TableRow} from 'grommet'
+
+const index = [
+  {
+    date:'02.06.1990',
+    title: 'Genesis',
+    description: 'Mauris ac dui ac ipsum egestas placerat. Fusce laoreet imperdiet sem. Praesent sit amet sapien at mi luctus maximus. Ut vel luctus justo.',
+    tags: 'abc, def, ghi, jkl',
+    target: '/genesis'
+  },
+  {
+    date:'02.06.1990',
+    title: 'Genesis',
+    description: 'Mauris ac dui ac ipsum egestas placerat. Fusce laoreet imperdiet sem. Praesent sit amet sapien at mi luctus maximus. Ut vel luctus justo.',
+    tags: 'abc, def, ghi, jkl',
+    target: '/genesis'
+  },
+  {
+    date:'02.06.1990',
+    title: 'Genesis',
+    description: 'Mauris ac dui ac ipsum egestas placerat. Fusce laoreet imperdiet sem. Praesent sit amet sapien at mi luctus maximus. Ut vel luctus justo.',
+    tags: 'abc, def, ghi, jkl',
+    target: '/genesis'
+  },
+  {
+    date:'02.06.1990',
+    title: 'Genesis',
+    description: 'Mauris ac dui ac ipsum egestas placerat. Fusce laoreet imperdiet sem. Praesent sit amet sapien at mi luctus maximus. Ut vel luctus justo.',
+    tags: 'abc, def, ghi, jkl',
+    target: '/genesis'
+  },
+  {
+    date:'02.06.1990',
+    title: 'Genesis',
+    description: 'Mauris ac dui ac ipsum egestas placerat. Fusce laoreet imperdiet sem. Praesent sit amet sapien at mi luctus maximus. Ut vel luctus justo.',
+    tags: 'abc, def, ghi, jkl',
+    target: '/genesis'
+  }
+]
+
 const IndexPage = () => (
-  <Layout>
+  <Grommet full>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+
+    <Heading level={3}>denntenna </Heading>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableCell scope='col' border='bottom'>
+            <Heading level={2}> Date </Heading>
+          </TableCell>
+          <TableCell scope='col' border='bottom'>
+            <Heading level={2}> Title </Heading>
+          </TableCell>
+          <TableCell scope='col' border='bottom'>
+            <Heading level={2}> Description </Heading>
+          </TableCell>
+          <TableCell scope='col' border='bottom'>
+            <Heading level={2}> Tags </Heading>
+          </TableCell>
+        </TableRow>
+      </TableHeader>
+
+      <TableBody>
+
+        {
+          index.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell scope='row'> <Text size={'medium'}> {item.date} </Text> </TableCell>
+                <TableCell scope='row'> 
+                  <Link to={`/${item.target}`}>
+                    <Text size={'medium'}> {item.title} </Text> 
+                  </Link>
+                </TableCell>
+                <TableCell scope='row'> <Text size={'medium'}> {item.description} </Text> </TableCell>
+                <TableCell scope='row'> <Text size={'medium'}> {item.tags} </Text> </TableCell>
+              </TableRow>
+          ))
+        }
+
+      </TableBody>
+    </Table>
+  </Grommet>
 )
 
 export default IndexPage
