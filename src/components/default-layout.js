a3975df455195f072413bd7f9bf8f9060e0a6d9d
denny <denny.george90@gmail.com>
2020-03-07 13:37:08 +0530

    (feat) styled visited links

--- src/components/default-layout.js
+++ src/components/default-layout.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import {Grommet, Box} from 'grommet'
+import React from 'react'
+import {Grommet, Box, Heading, Text } from 'grommet'
 import Logo from './logo';
 import SEO from './seo';
 import Theme from './theme';
+import {Link} from 'gatsby'
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+    box-shadow: none;
+`
 
 /**
 * @author
@@ -10,23 +16,27 @@ import Theme from './theme';
 **/
 
 const DefaultLayout = ({children}) => {
-    const [fetching, setFetching] = useState(false)
-
-    useEffect(()=> {
-        setFetching(true)
-    })
 
     return (
         <Grommet theme={Theme} full>
-            <Box direction={'column'}>
-                <SEO title="Home" />
 
-                <Logo/>
+            <Box fill direction={'column'}>
+                <Box flex={false}>
+                    <SEO title="Home" />
+                    <StyledLink to={'/'}>
+                        <Logo/>
+                    </StyledLink>
+                </Box>
 
-                <Box pad={'medium'}>
+                <Box flex overflow={'auto'} pad={'medium'}>
                     {children}
                 </Box>
+
+                <Box flex={false} pad={'medium'}>
+                    <Heading level={5} margin={'none'}> footer </Heading>
+                </Box>
             </Box>
+
         </Grommet>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 import {Grommet, Box, Heading, Text, Table, TableHeader, TableBody, TableCell, TableRow} from 'grommet'
