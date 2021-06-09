c4ac688cdbfcbf715be0987f1d2a0f1416ea5e1a
dennyabrain <denny.george90@gmail.com>
2021-06-09 23:37:58 +0530

    feat: add generative icons for each type of garden on the homepage

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ run `npm run deploy` to push changes to github
 check site on denntenna.github.io
 
 Take notes often. Publish when ready :)
+
+## Attribution
+
+Plant by TRAVIS BIRD from the Noun Project
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,107 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import { Grommet, Box, Text } from "grommet"
 import SEO from "../components/seo"
+import Theme from "../components/theme"
+import GardenIcon from "../components/GardenIcon"
 
-import {Grommet, Box, Heading, Text, Table, TableHeader, TableBody, TableCell, TableRow} from 'grommet'
-import Theme from '../components/theme';
-import Logo from '../components/logo';
-import { PlainExternalLink } from '../components/Links';
-
-const index = [
-  {
-    date:'27.03.2020',
-    title: 'Khoj',
-    description: 'A search engine for fact check articles',
-    tags: 'technologist, ux, pm',
-    target: 'https://services.tattle.co.in/khoj'
-  }
-]
-
-const PaddedContainer = ({children}) => (
-  <Box pad={{left:'medium', right:'medium'}}>
-    {children}
-  </Box>
-)
-
-
-const TableHeading = ({label}) => (
-  <PaddedContainer>
-    <Heading level={3}> {label} </Heading>
-  </PaddedContainer>
-)
-
-const TableItem = ({label}) => (
-  <PaddedContainer>
-    <Text size={'medium'}> {label} </Text>
-  </PaddedContainer>
-)
-
-const TableLink = ({target, label}) => (
-  <PaddedContainer>
-    {
-      target.startsWith('http') ?
-      <PlainExternalLink href={target} target={'_blank'}>
-        <Text size={'medium'}> {label} </Text> 
-      </PlainExternalLink>
-      :
-      <Link to={`/${target}`}>
-        <Text size={'medium'}> {label} </Text> 
-      </Link>
-    }
-  </PaddedContainer>
-)
-
-const IndexPage = () => (
-  <Grommet theme={Theme} full>
-    <SEO title="Home" />
-
-    <Logo/>
-    
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableCell scope='col' border='bottom'>
-            <TableHeading label={'Date'}/>
-          </TableCell>
-          <TableCell scope='col' border='bottom'>
-            <TableHeading label={'Title'}/>
-          </TableCell>
-          <TableCell scope='col' border='bottom'>
-            <TableHeading label={'Description'}/>
-          </TableCell>
-          <TableCell scope='col' border='bottom'>
-            <TableHeading label={'Tags'}/>
-          </TableCell>
-        </TableRow>
-      </TableHeader>
-
-      <TableBody>
-
-        {
-          index.map((item, index) => (
-              <TableRow key={index}>
-                <TableCell scope='row'> 
-                  <TableItem label={item.date} /> 
-                </TableCell>
-                <TableCell scope='row'> 
-                  <TableLink 
-                    target={`${item.target}`}
-                    label={item.title}
-                  />
-                </TableCell>
-                <TableCell scope='row'> 
-                  <TableItem label={item.description} /> 
-                </TableCell>
-                <TableCell scope='row'> 
-                  <TableItem label={item.tags} /> 
-                </TableCell>
-              </TableRow>
-          ))
-        }
-
-      </TableBody>
-    </Table>
-  </Grommet>
-)
+const IndexPage = () => {
+  return (
+    <Grommet theme={Theme} full>
+      <SEO title="Home" />
+      <Box fill>
+        <Box direction={"row"} wrap={true}>
+          {Array.from({ length: 100 }, (v, i) =>
+            Math.floor(Math.random() * 4)
+          ).map((i, ix) => (
+            <Box>
+              <GardenIcon key={ix} size={40} seed={Math.random() * 4} />
+              <Text textAlign={"center"} size={"small"}>
+                Post Title
+              </Text>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Grommet>
+  )
+}
 
 export default IndexPage
