5f5a69b008ccb132fab8faf3530aa2378f235e0a
dennyabrain <denny.george90@gmail.com>
2021-11-17 14:31:38 +0530

    feat : add an about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Heading, Paragraph, Text } from "grommet";
+import DefaultMDXLayout from "../components/default-mdx-layout";
+import { StaticImage } from "gatsby-plugin-image";
+
+const AboutPage = () => {
+	return (
+		<DefaultMDXLayout>
+			<Box>
+				<Heading level={2}>About</Heading>
+				<StaticImage
+					src="../images/about_profile_pic.jpg"
+					alt={"An image of the author in a park"}
+				/>
+			</Box>
+			<Paragraph>Hi,</Paragraph>
+			<Paragraph>
+				My name is denny. I am a technologist and humorist.
+			</Paragraph>
+			<Paragraph>
+				I created this digital space to experiment with online content
+				creation. We've seen a lot of different kind of content
+				proliferate on social media, but somehow most websites and blog
+				don't really utilize the power of web technologies. The reason
+				is of course that there is building interesting experiences on
+				web still requires coding and that could be prohibitive to a lot
+				of people. I intend to disregard any concerns of scalability and
+				experiment with web technologies to learn how independent
+				creators could also carve their unique space online without
+				relying on social media platforms.
+			</Paragraph>
+		</DefaultMDXLayout>
+	);
+};
+
+export default AboutPage;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,54 +6,56 @@ import Section from "../components/section";
 import { Link } from "gatsby";
 
 const IndexPage = () => (
-  <DefaultMDXLayout>
-    <Box fill>
-      <Box height={"2em"}></Box>
-      <Box direction={"column"} gap={"medium"} flex>
-        <Section>
-          <Box>
-            <Heading level={2} margin={"none"}>
-              Series
-            </Heading>
-            <Text size={"medium"} weight={400}>
-              Opinions in Formation, Chronospatial and 2 others
-            </Text>
-          </Box>
-        </Section>
-        <Section>
-          <Heading level={2} margin={"none"}>
-            Cheatsheets
-          </Heading>
-          <Text size={"medium"}>
-            <Link to={"/cheatsheets/elasticsearch"}>Elasticsearch</Link> Dart,
-            flutter and 7 others
-          </Text>
-        </Section>
-        <Section>
-          <Heading level={2} margin={"none"}>
-            Reading List
-          </Heading>
-          <Text size={"medium"}>
-            Braided River, ISIS caliphate, Convinience Store Woman,{" "}
-            <Link to={"/reading-notes/democracy-is-sentimental"}>
-              Democracy is sentimental
-            </Link>
-            {", "}
-            <Link to={"/reading-notes/when-hope-is-a-hindrance"}>
-              When hope is a hindrance
-            </Link>{" "}
-            and 10 others
-          </Text>
-        </Section>
-        <Section>
-          <Heading level={2} margin={"none"}>
-            Portfolio
-          </Heading>
-          <Text size={"medium"}>Dhish, Migracam and Tattle</Text>
-        </Section>
-      </Box>
-    </Box>
-  </DefaultMDXLayout>
+	<DefaultMDXLayout>
+		<Box fill>
+			<Box height={"2em"}></Box>
+			<Box direction={"column"} gap={"medium"} flex>
+				<Section>
+					<Box>
+						<Heading level={2} margin={"none"}>
+							Series
+						</Heading>
+						<Text size={"medium"} weight={400}>
+							Opinions in Formation, Chronospatial and 2 others
+						</Text>
+					</Box>
+				</Section>
+				<Section>
+					<Heading level={2} margin={"none"}>
+						Cheatsheets
+					</Heading>
+					<Text size={"medium"}>
+						<Link to={"/cheatsheets/elasticsearch"}>
+							Elasticsearch
+						</Link>{" "}
+						Dart, flutter and 7 others
+					</Text>
+				</Section>
+				<Section>
+					<Heading level={2} margin={"none"}>
+						Reading Notes
+					</Heading>
+					<Text size={"medium"}>
+						Braided River, ISIS caliphate, Convinience Store Woman,{" "}
+						<Link to={"/reading-notes/democracy-is-sentimental"}>
+							Democracy is sentimental
+						</Link>
+						{", "}
+						<Link to={"/reading-notes/when-hope-is-a-hindrance"}>
+							When hope is a hindrance
+						</Link>{" "}
+						and 10 others
+					</Text>
+				</Section>
+				<Section>
+					<Heading level={2} margin={"none"}>
+						Portfolio
+					</Heading>
+					<Text size={"medium"}>Dhish, Migracam and Tattle</Text>
+				</Section>
+			</Box>
+		</Box>
+	</DefaultMDXLayout>
 );
 
 export default IndexPage;
