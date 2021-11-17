936ec161f72e670c7cd15b58630154883e3be201
dennyabrain <denny.george90@gmail.com>
2021-11-18 00:13:58 +0530

    feat : style oif

--- src/components/theme.js
+++ src/components/theme.js
@@ -1,27 +1,30 @@
 const Theme = {
-  global: {
-    font: {
-      family: "Noto Serif Display",
-    },
-    colors: {
-      background: "#ffffff",
-      brand: "#3d405b",
-      "accent-1": "#81b29a",
-      "neutral-1": "#f2cc8f",
-      "neutral-2": "#a8dadc",
-      text: "#005f73",
-    },
-  },
-  text: {
-    font: {
-      family: "Noto Sans",
-    },
-  },
-  paragraph: {
-    font: {
-      family: "Noto Sans",
-    },
-  },
+	global: {
+		font: {
+			family: "Noto Serif Display",
+		},
+		colors: {
+			background: "#ffffff",
+			brand: "#3d405b",
+			"accent-1": "#81b29a",
+			"neutral-1": "#f2cc8f",
+			"neutral-2": "#a8dadc",
+			text: {
+				light: "#005f73",
+				dark: "#ffffff",
+			},
+		},
+	},
+	text: {
+		font: {
+			family: "Noto Sans",
+		},
+	},
+	paragraph: {
+		font: {
+			family: "Noto Sans",
+		},
+	},
 };
 
 export default Theme;
--- src/pages/series/oif.jsx
+++ src/pages/series/oif.jsx
@@ -1,27 +1,95 @@
 import React from "react";
-import { Box, Heading, Paragraph, Text, Anchor } from "grommet";
-import DefaultMDXLayout from "../../components/default-mdx-layout";
+import {
+	Grommet,
+	Box,
+	Stack,
+	Heading,
+	Content,
+	Paragraph,
+	Text,
+	Anchor,
+} from "grommet";
 import { StaticImage } from "gatsby-plugin-image";
+import Theme from "../../components/theme";
+import { Link } from "gatsby";
 
 const OIFPage = () => {
 	return (
-		<DefaultMDXLayout>
-			<Box>
-				<Heading level={2}>Opinions in Formation</Heading>
-				<StaticImage
-					src="../../images/oif_cover.jpeg"
-					alt={
-						"An artwork by Tanaka Atsuko featuring colored circles and smooth curves overlayed on each other"
-					}
-				/>
+		<Grommet full theme={Theme}>
+			<Box fill flex={"grow"} gap={"small"}>
+				<Box
+					height={{ min: "100vh" }}
+					background={"red"}
+					overflow={"hidden"}
+				>
+					<Stack fill>
+						<Box height={{ min: "100vh" }} background={"aqua"}>
+							<StaticImage
+								src={"../../images/oif_cover.jpeg"}
+								alt={
+									"An artwork by Tanaka Atsuko featuring colored circles and smooth curves overlayed on each other"
+								}
+								objectFit={"cover"}
+							/>
+						</Box>
+
+						<Box fill justify={"center"} background={"#00000044"}>
+							<Heading
+								level={1}
+								alignSelf={"center"}
+								color={"light-1"}
+							>
+								Opinions in Formation
+							</Heading>
+							<Paragraph alignSelf={"center"} color={"light-1"}>
+								This section of live documents that show my
+								current understanding of contentious issues. I
+								see some value in sharing how I've been thinking
+								about some of these things. I collect literature
+								that I have reffered to and provide my notes and
+								commntary on it. I also invite additional link
+								and resource submissions from readers.
+							</Paragraph>
+						</Box>
+						<Box pad={"medium"}>
+							<Box
+								direction={"row-responsive"}
+								gap={"medium"}
+								align={"baseline"}
+							>
+								<Link to="/">
+									<Heading margin={"none"} level={4}>
+										denntenna
+									</Heading>
+								</Link>
+								<Box gap={"small"}>
+									<Link to={"/about"}>
+										<Text size={"small"}>about</Text>
+									</Link>
+								</Box>
+							</Box>
+							<Box height={"0.2em"} />
+							<Box
+								background={"neutral-2"}
+								round={"xsmall"}
+								pad={"xsmall"}
+								width={"fit-content"}
+							>
+								<Text size={"small"}>
+									a digital garden, feed, portfolio and
+									experiment in content creation
+								</Text>
+							</Box>
+						</Box>
+					</Stack>
+				</Box>
+				<Box pad={"large"}>
+					<Heading level={2}>Synthetic Media</Heading>
+					<Heading level={2}>Aryan Invasion Theory</Heading>
+					<Heading level={2}>Internet Shutdowns</Heading>
+				</Box>
 			</Box>
-			<Text size={"small"} color={"dark-2"}>
-				Painting by Tanaka Atsuko found{" "}
-				<Anchor href={"https://www.artic.edu/artworks/229393/untitled"}>
-					here
-				</Anchor>
-			</Text>
-		</DefaultMDXLayout>
+		</Grommet>
 	);
 };
 
