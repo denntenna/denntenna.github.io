d98f80b0a7f59f6e8be35b62f6324ad993eaa2be
Denny <denny.george90@gmail.com>
2022-06-10 11:18:46 +0530

    feat: add listing for fullstack role

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,13 +8,26 @@ import { Link } from "gatsby";
 const IndexPage = () => (
   <DefaultMDXLayout>
     <Box fill>
+      <Box pad={"medium"} round border width={"fit-content"}>
+        <Text>
+          I am currently hiring a Full stack javascript developer for Tattle
+        </Text>{" "}
+        <Anchor
+          href={"https://tattle.co.in/career/fullstack/"}
+          target={"_blank"}
+        >
+          Check out the Job Listing
+        </Anchor>
+      </Box>
       <Box height={"2em"}></Box>
+
       <Box direction={"column"} gap={"medium"} flex>
         <Section>
           <Heading level={2} margin={"none"}>
             Portfolio
           </Heading>
           <Box height="0.8em"></Box>
+
           <Box direction={"column"} gap={"xsmall"}>
             <Text>
               <Anchor href={"https://tattle.co.in"} target={"blank"}>
@@ -74,50 +87,43 @@ const IndexPage = () => (
             </Text>
           </Box>
         </Section>
-        {/*<Section>
-					<Box>
-						<Heading level={2} margin={"none"}>
-							Series
-						</Heading>
-						<Text size={"medium"} weight={400}>
-							<Link to={"/series/oif"}>
-								Opinions in Formation
-							</Link>{" "}
-							,{" "}
-							<Link to={"/series/chronospatial"}>
-								Chronospatial
-							</Link>{" "}
-							and 2 others
-						</Text>
-					</Box>
-				</Section>
-				<Section>
-					<Heading level={2} margin={"none"}>
-						Cheatsheets
-					</Heading>
-					<Text size={"medium"}>
-						<Link to={"/cheatsheets/elasticsearch"}>
-							Elasticsearch
-						</Link>{" "}
-						Dart, flutter and 7 others
-					</Text>
-				</Section>
-				<Section>
-					<Heading level={2} margin={"none"}>
-						Reading Notes
-					</Heading>
-					<Text size={"medium"}>
-						Braided River, ISIS caliphate, Convinience Store Woman,{" "}
-						<Link to={"/reading-notes/democracy-is-sentimental"}>
-							Democracy is sentimental
-						</Link>
-						{", "}
-						<Link to={"/reading-notes/when-hope-is-a-hindrance"}>
-							When hope is a hindrance
-						</Link>{" "}
-						and 10 others
-					</Text>
-				</Section> */}
+        <Section>
+          {/* <Box>
+            <Heading level={2} margin={"none"}>
+              Series
+            </Heading>
+            <Text size={"medium"} weight={400}>
+              <Link to={"/series/oif"}>Opinions in Formation</Link> ,{" "}
+              <Link to={"/series/chronospatial"}>Chronospatial</Link> and 2
+              others
+            </Text>
+          </Box> */}
+        </Section>
+        <Section>
+          <Heading level={2} margin={"none"}>
+            Cheatsheets
+          </Heading>
+          <Text size={"medium"}>
+            <Link to={"/cheatsheets/elasticsearch"}>Elasticsearch</Link> Dart,
+            flutter and 7 others
+          </Text>
+        </Section>
+        <Section>
+          <Heading level={2} margin={"none"}>
+            Reading Notes
+          </Heading>
+          <Text size={"medium"}>
+            Braided River, ISIS caliphate, Convinience Store Woman,{" "}
+            <Link to={"/reading-notes/democracy-is-sentimental"}>
+              Democracy is sentimental
+            </Link>
+            {", "}
+            <Link to={"/reading-notes/when-hope-is-a-hindrance"}>
+              When hope is a hindrance
+            </Link>{" "}
+            and 10 others
+          </Text>
+        </Section>
       </Box>
     </Box>
   </DefaultMDXLayout>
