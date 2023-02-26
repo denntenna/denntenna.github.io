c69e8dd9c2da7e76b201934884d5259e312eee74
dennyabrain <denny.george90@gmail.com>
2023-02-26 11:30:47 +0530

    feat: add new entries to reading note and created index page

---
+++ src/pages/reading-notes/index.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { graphql, Link } from "gatsby";
+import DefaultMDXLayout from "../../components/default-mdx-layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { Box, Heading, Text, Anchor, Image } from "grommet";
+
+const ReadingNotes = ({ data }) => {
+  const posts = data.allMdx.nodes;
+
+  console.log(data);
+  return (
+    <DefaultMDXLayout>
+      <Box>
+        <Heading level={2}>Reading Notes</Heading>
+        <Box>
+          <Box width={"100%"} height={"fit-content"}>
+            <StaticImage
+              src="../../images/rn_cover.jpg"
+              alt={"A picture of an old cinema theatre"}
+            />
+          </Box>
+          <Text size={"small"} color={"dark-2"}>
+            Picture by Walker Evans -{" "}
+            <Anchor
+              href={"https://www.artic.edu/artworks/33833/untitled-cinema"}
+            >
+              source
+            </Anchor>
+          </Text>
+        </Box>
+      </Box>
+
+      <Box gap={"xsmall"} margin={{ top: "medium" }}>
+        {posts
+          ? posts.map((post, ix) => {
+              const { frontmatter } = post;
+              const featuredImage = frontmatter.featuredimage;
+              // console.log({
+              //   title: frontmatter.title,
+              //   image: featuredImage.src,
+              // });
+              let featuredImageSrc = featuredImage
+                ? featuredImage.src.childImageSharp.fluid.src
+                : undefined;
+
+              return (
+                <Box
+                  key={ix}
+                  direction={"row-responsive"}
+                  gap={"small"}
+                  pad={"xsmall"}
+                  background={ix % 2 === 0 ? "light-1" : "light-3"}
+                >
+                  {featuredImageSrc ? (
+                    <Box height={"xsmall"} width={"xsmall"} round>
+                      <Image
+                        fill
+                        fit="cover"
+                        src={
+                          post.frontmatter.featuredimage.src.childImageSharp
+                            .fluid.src
+                        }
+                        alt={"Thumbnail image"}
+                      />
+                    </Box>
+                  ) : null}
+                  <Box>
+                    <Anchor href={`/${post.slug}`}>
+                      <Heading level={4} margin={"none"}>
+                        {frontmatter.title}
+                      </Heading>
+                    </Anchor>
+                    <Text> {frontmatter.author}</Text>
+                    <Text>{frontmatter.description}</Text>
+                    <Text size={"xsmall"}>{frontmatter.date}</Text>
+                  </Box>
+                </Box>
+              );
+            })
+          : null}
+      </Box>
+    </DefaultMDXLayout>
+  );
+};
+
+export const query = graphql`
+  query RNFeedIndexQuery {
+    allMdx(
+      filter: { fileAbsolutePath: { regex: "/.*/src/pages/reading-notes/" } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        slug
+        frontmatter {
+          title
+          author
+          featuredimage {
+            src {
+              childImageSharp {
+                fluid(maxWidth: 1024) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            alt
+          }
+          date
+        }
+        fileAbsolutePath
+      }
+    }
+  }
+`;
+
+export default ReadingNotes;
