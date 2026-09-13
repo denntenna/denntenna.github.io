eded2655f421509789ce4096882f5d821c4e7843
dennyabrain <denny.george90@gmail.com>
2026-09-14 00:15:33 +0530

    fix: text changes

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -12,15 +12,13 @@ function Page() {
           Portfolio
         </Heading>
 
-        <Box direction={"column"}>
-          <Box>
+        <Box direction={"column"} gap="small">
+          <Box gap="xsmall">
             <Text>
-              <Anchor href={"https://tattle.co.in"} target={"blank"}>
-                Tattle
-              </Anchor>
-              {" (2020 onwards) : Tools and Dataset to understand and respond to misinformation in India."}
+              <Anchor href={"https://tattle.co.in"} target={"blank"}>Tattle</Anchor>
+              {" (2020 onwards) : Open Source Software and datasets to understand and respond to online harms in India."}
             </Text>
-            <Box margin={{ left: "medium", top: "xsmall" }} gap="xsmall">
+            <Box margin={{ left: "medium" }} gap="xsmall">
               <Text size="small">
                 <Anchor href={"https://tattle.co.in/products/ogbv/"} target={"blank"}>Uli</Anchor>
                 {" — Reduces effects of Online Gender-Based Violence with datasets and software for collective response."}
@@ -40,59 +38,23 @@ function Page() {
             </Box>
           </Box>
           <Text>
-            <Anchor
-              href={"https://www.instagram.com/mouthwash.reels/reels/"}
-              target={"blank"}
-            >
-              {" "}
-              Mouthwash
-            </Anchor>
-            {" (2022) : Comedy Sketches on Instagram Reels"}
-          </Text>
-
-
-
-          <Text>
-            <Anchor
-              href={
-                "https://winners.webbyawards.com/2019/apps-and-software/general-mobile-ott-apps/public-service-activism/78754/migracam"
-              }
-              target={"blank"}
-            >
-              {" "}
-              Migracam
-            </Anchor>
-            {
-              " (2020) : An app for immigrants on US border to record law enforcement misdemeanor. Built at Quardrant2"
-            }
+            <Anchor href={"https://www.instagram.com/mouthwash.reels/reels/"} target={"blank"}>Mouthwash</Anchor>
+            {" (2022) : Comedy sketches on Instagram Reels."}
           </Text>
           <Text>
-            <Anchor
-              href={"https://www.youtube.com/watch?v=yvUFGLoQxfI"}
-              target={"blank"}
-            >
-              {" "}
-              Dhish
-            </Anchor>
+            <Anchor href={"https://winners.webbyawards.com/2019/apps-and-software/general-mobile-ott-apps/public-service-activism/78754/migracam"} target={"blank"}>Migracam</Anchor>
+            {" (2020) : An app for immigrants on the US border to record law enforcement misdemeanor. Built at Quadrant2."}
+          </Text>
+          <Text>
+            <Anchor href={"https://www.youtube.com/watch?v=yvUFGLoQxfI"} target={"blank"}>Dhish</Anchor>
             {" (2019) : Make music with intuitive percussive hand gestures."}
           </Text>
           <Text>
-            <Anchor
-              href={
-                "https://www.vice.com/en/article/kbnbvn/this-invention-lets-your-pizza-tweet-every-time-you-take-a-bite"
-              }
-              target={"blank"}
-            >
-              {" "}
-              Tweat
-            </Anchor>
-            {" (2016) : Live Tweet every bite of your food"}
+            <Anchor href={"https://www.vice.com/en/article/kbnbvn/this-invention-lets-your-pizza-tweet-every-time-you-take-a-bite"} target={"blank"}>Tweat</Anchor>
+            {" (2016) : Live tweet every bite of your food."}
           </Text>
           <Text>
-            <Link to={"/standup-comedy-2014"}>
-              {" "}
-              Standup Comedy
-            </Link>
+            <Link to={"/standup-comedy-2014"}><Text weight="bold">Standup Comedy</Text></Link>
             {" (2014) : Performing standup comedy in New Delhi's burgeoning comedy scene."}
           </Text>
         </Box>
@@ -103,7 +65,7 @@ function Page() {
         </Heading>
 
         <Link to={"/generative-workout-mix"}>
-          <Text size="medium">Live Coding Workout Mix</Text>
+          <Text size="medium" weight="bold">Live Coding Workout Mix</Text>
         </Link>
       </Section>
     </IndexLayout>
